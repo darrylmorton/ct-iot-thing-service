71bf69faca3c05b8a732aa147b62773593be9195
Darryl Morton <darryl.j.morton@gmail.com>
2022-08-24 20:33:01 +0100

    Revert "version bump"
    This reverts commit e0b7a913119d5fa6095d4b7747bd17e0877a0ca2.

--- seeds/things.ts
+++ seeds/things.ts
@@ -2,38 +2,21 @@ import { client } from '../src/db'
 import { ThingPayload } from '../src/types'
 import { createThingPayload } from '../test/helper/thingHelper'
 
-const PAYLOADS_TOTAL: number = 1000
-const thingNames: Array<string> = ['thingOne', 'thingTwo', 'thingThree', 'thingFour', 'thingFive', 'thingSix']
-const thingTypeNames: Array<string> = ['thingTypeOne', 'thingTypeTwo', 'thingTypeThree']
+const PAYLOADS_TOTAL = 1000
 
 export const seed = async (): Promise<void> => {
   await cleanup()
 
-  await client('thing_types')
-    .insert([{ name: thingTypeNames[0] }, { name: thingTypeNames[1] }, { name: thingTypeNames[2] }])
-    .returning(['name'])
+  const [{ name: thingTypeName }] = await client('thing_types').insert({ name: 'thingTypeOne' }).returning(['name'])
 
-  const [
-    { id: thingOneId },
-    { id: thingTwoId },
-    { id: thingThreeId },
-    { id: thingFourId },
-    { id: thingFiveId },
-    { id: thingSixId },
-  ] = await client('things')
-    .insert([
-      { name: thingNames[0], thing_type: thingTypeNames[0] },
-      { name: thingNames[1], thing_type: thingTypeNames[0] },
-      { name: thingNames[2], thing_type: thingTypeNames[1] },
-      { name: thingNames[3], thing_type: thingTypeNames[1] },
-      { name: thingNames[4], thing_type: thingTypeNames[2] },
-      { name: thingNames[5], thing_type: thingTypeNames[2] },
-    ])
+  const [{ id: thingId }] = await client('things')
+    .insert({ name: 'thingOne', thing_type: thingTypeName })
     .returning(['id'])
 
   for (let payloadsCounter = 0; payloadsCounter < PAYLOADS_TOTAL; payloadsCounter++) {
-    const thingOnePayload: ThingPayload = createThingPayload(thingOneId, payloadsCounter)
-    await client('thing_payloads').insert(thingOnePayload)
+    const thingPayload: ThingPayload = createThingPayload(thingId, payloadsCounter)
+
+    await client('thing_payloads').insert(thingPayload)
   }
 }
 
