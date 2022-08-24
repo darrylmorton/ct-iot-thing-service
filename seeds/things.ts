4e5085d62a767273bfd24dca4cdf0d3da2793e1a
Darryl Morton <darryl.j.morton@gmail.com>
2022-08-24 20:47:19 +0100

    more things, thing types and payloads seed data

--- seeds/things.ts
+++ seeds/things.ts
@@ -1,22 +1,50 @@
+// eslint-disable no-unused-var
 import { client } from '../src/db'
 import { ThingPayload } from '../src/types'
 import { createThingPayload } from '../test/helper/thingHelper'
 
-const PAYLOADS_TOTAL = 1000
+const PAYLOADS_TOTAL: number = 100
+const thingNames: Array<string> = ['thingOne', 'thingTwo', 'thingThree', 'thingFour', 'thingFive', 'thingSix']
+const thingTypeNames: Array<string> = ['thingTypeOne', 'thingTypeTwo', 'thingTypeThree']
+
+const insertThingPayload = async (thingId: string, index: number): Promise<void> => {
+  const thingOnePayload: ThingPayload = createThingPayload(thingId, index)
+
+  await client('thing_payloads').insert(thingOnePayload)
+}
 
 export const seed = async (): Promise<void> => {
   await cleanup()
 
-  const [{ name: thingTypeName }] = await client('thing_types').insert({ name: 'thingTypeOne' }).returning(['name'])
+  await client('thing_types')
+    .insert([{ name: thingTypeNames[0] }, { name: thingTypeNames[1] }, { name: thingTypeNames[2] }])
+    .returning(['name'])
 
-  const [{ id: thingId }] = await client('things')
-    .insert({ name: 'thingOne', thing_type: thingTypeName })
+  const [
+    { id: thingOneId },
+    { id: thingTwoId },
+    { id: thingThreeId },
+    { id: thingFourId },
+    { id: thingFiveId },
+    { id: thingSixId },
+  ] = await client('things')
+    .insert([
+      { name: thingNames[0], thing_type: thingTypeNames[0] },
+      { name: thingNames[1], thing_type: thingTypeNames[0] },
+      { name: thingNames[2], thing_type: thingTypeNames[1] },
+      { name: thingNames[3], thing_type: thingTypeNames[1] },
+      { name: thingNames[4], thing_type: thingTypeNames[2] },
+      { name: thingNames[5], thing_type: thingTypeNames[2] },
+    ])
     .returning(['id'])
 
   for (let payloadsCounter = 0; payloadsCounter < PAYLOADS_TOTAL; payloadsCounter++) {
-    const thingPayload: ThingPayload = createThingPayload(thingId, payloadsCounter)
-
-    await client('thing_payloads').insert(thingPayload)
+    await insertThingPayload(thingOneId, payloadsCounter)
+    await insertThingPayload(thingTwoId, payloadsCounter)
+    await insertThingPayload(thingThreeId, payloadsCounter)
+    await insertThingPayload(thingFourId, payloadsCounter)
+    await insertThingPayload(thingFiveId, payloadsCounter)
+    await insertThingPayload(thingSixId, payloadsCounter)
   }
 }
 
