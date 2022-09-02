f87f4ef4415a04b4c9eaf82075ba1c8d493daf47
Darryl Morton <darryl.j.morton@gmail.com>
2022-09-02 21:54:45 +0100

    dockerfile cleanup, version bump, docker ignore, env cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,10 @@
-FROM node:16.15.0-alpine AS build
-
-ARG LOGLEVEL
-ENV NPM_CONFIG_LOGLEVEL ${LOGLEVEL}
+FROM node:16.15.0-alpine
 
 WORKDIR /ct-iot-thing-service
 
 COPY . .
 
 RUN npm install -g npm@8.x.x
-RUN npm ci
-RUN npm run build
-
-#######################################################################
-FROM build
-
-COPY --from=build /ct-iot-thing-service/dist /ct-iot-thing-service/dist
-COPY --from=build /ct-iot-thing-service/package* /ct-iot-thing-service
-
 RUN npm ci --production
 
 EXPOSE 3002
--- src/env.ts
+++ src/env.ts
@@ -9,12 +9,11 @@ if (process.env.NODE_ENV === 'test') {
 
 const env = cleanEnv(process.env, {
   LOG_LEVEL: str({ default: 'info', devDefault: 'debug' }),
-  PORT: port({ devDefault: 3002 }),
+  PORT: port({ default: 3002 }),
   API_VERSION: str({ default: version }),
   API_MAJOR_VERSION: str({ default: 'v1' }),
-  JWT_SECRET: str({ devDefault: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321' }),
   DB_HOST: str({ devDefault: 'localhost' }),
-  DB_PORT: num({ devDefault: 5432 }),
+  DB_PORT: num({ default: 5432 }),
   DB_USERNAME: str({ devDefault: 'postgres' }),
   DB_PASSWORD: str({ devDefault: 'postgres' }),
   DB_NAME: str({ default: 'things' }),
