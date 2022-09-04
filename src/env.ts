91cf08dc1f4e4d252c436a273d33230eaf099df4
Darryl Morton <darryl.j.morton@gmail.com>
2022-09-04 17:51:20 +0100

    ssm envs

--- src/env.ts
+++ src/env.ts
@@ -2,33 +2,9 @@ import { cleanEnv, str, port, host } from 'envalid'
 import dotenv from 'dotenv'
 
 import { version } from '../package.json'
-import { GetSecretValueCommand, SecretsManagerClient } from '@aws-sdk/client-secrets-manager'
-import logger from './logger'
 
 if (process.env.NODE_ENV === 'test') {
   dotenv.config({ path: './test/test.env' })
-} else if (process.env.NODE_ENV === 'production') {
-  const setEnvs = async () => {
-    const secretsManagerClient = new SecretsManagerClient({
-      region: process.env.AWS_REGION,
-    })
-    const secretsManagerCommand = new GetSecretValueCommand({
-      SecretId: process.env.THINGS_SECRETS_NAME,
-    })
-
-    try {
-      const secretsManagerResult = await secretsManagerClient.send(secretsManagerCommand)
-      const dbSecrets: any = JSON.parse(<string>secretsManagerResult.SecretString)
-
-      process.env.DB_USERNAME = dbSecrets.username
-      process.env.DB_PASSWORD = dbSecrets.password
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      logger.error(`ThingServiceSecretsManagerError ${err}`)
-    }
-  }
-
-  setEnvs().then((done) => done)
 }
 
 const env = cleanEnv(process.env, {
