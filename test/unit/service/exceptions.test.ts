466377f0665c4e06852914bfe607071438d934d9
darrylmorton <darryl.j.morton@gmail.com>
2024-07-12 11:53:04 +0100

    thing type service unit tests - happy paths

--- test/unit/service/exceptions.test.ts
+++ test/unit/service/exceptions.test.ts
@@ -11,8 +11,9 @@ import {
   ServiceThingResponse,
   ServiceThingTypeResponse,
 } from '../../../src/types/serviceTypes'
+import db from '../../../src/db'
 
-describe('Service', () => {
+describe.only('Service', () => {
   beforeEach(() => {
     sinon.restore()
   })
@@ -104,12 +105,53 @@ describe('Service', () => {
       expect(actualResult.result).to.deep.equal({})
     })
 
+    it('get by name - missing', async () => {
+      sinon.stub(db, 'findThingTypeByName').returns(Promise.resolve([]))
+
+      const spy: sinon.SinonSpy<[name: string], Promise<ServiceThingTypeResponse>> = sinon.spy(
+        thingService,
+        'getThingTypeByName'
+      )
+
+      const actualResult = await spy('thingType')
+
+      expect(actualResult.statusCode).to.equal(404)
+      expect(actualResult.result).to.deep.equal({})
+    })
+
+    it('get by name - success', async () => {
+      const expectedResult = { name: 'thingType', description: 'thingType' }
+
+      sinon.stub(db, 'findThingTypeByName').returns(Promise.resolve([expectedResult]))
+
+      const spy: sinon.SinonSpy<[name: string], Promise<ServiceThingTypeResponse>> = sinon.spy(
+        thingService,
+        'getThingTypeByName'
+      )
+
+      const actualResult = await spy('thingType')
+
+      expect(actualResult.statusCode).to.equal(200)
+      expect(actualResult.result).to.deep.equal(expectedResult)
+    })
+
     it('get all', async () => {
       const spy: sinon.SinonSpy<[], Promise<ServiceThingTypeResponse>> = sinon.spy(thingService, 'getThingTypes')
 
       const actualResult = await spy()
 
       expect(actualResult.statusCode).to.equal(500)
+      expect(actualResult.result).to.deep.equal([])
+    })
+
+    it('get all success', async () => {
+      sinon.stub(db, 'findThingTypes').returns(Promise.resolve([]))
+
+      const spy: sinon.SinonSpy<[], Promise<ServiceThingTypeResponse>> = sinon.spy(thingService, 'getThingTypes')
+
+      const actualResult = await spy()
+
+      expect(actualResult.statusCode).to.equal(200)
       expect(actualResult.result).to.deep.equal([])
     })
 
@@ -123,6 +165,39 @@ describe('Service', () => {
 
       expect(actualResult.statusCode).to.equal(500)
       expect(actualResult.result).to.deep.equal({})
+    })
+
+    it('post - exists', async () => {
+      const expectedResult = { name: 'thingType', description: 'thingType' }
+
+      sinon.stub(db, 'findThingTypeByName').returns(Promise.resolve([expectedResult]))
+
+      const spy: sinon.SinonSpy<[thingType: ThingType], Promise<ServiceThingTypeResponse>> = sinon.spy(
+        thingService,
+        'postThingType'
+      )
+
+      const actualResult = await spy({ name: 'thingType', description: 'thingType' })
+
+      expect(actualResult.statusCode).to.equal(409)
+      expect(actualResult.result).to.deep.equal({})
+    })
+
+    it('post - success', async () => {
+      const expectedResult = { name: 'thingType', description: 'thingType' }
+
+      sinon.stub(db, 'findThingTypeByName').returns(Promise.resolve([]))
+      sinon.stub(db, 'addThingType').returns(Promise.resolve([expectedResult]))
+
+      const spy: sinon.SinonSpy<[thingType: ThingType], Promise<ServiceThingTypeResponse>> = sinon.spy(
+        thingService,
+        'postThingType'
+      )
+
+      const actualResult = await spy({ name: 'thingType', description: 'thingType' })
+
+      expect(actualResult.statusCode).to.equal(201)
+      expect(actualResult.result).to.deep.equal(expectedResult)
     })
   })
 
